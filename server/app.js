ac5f64ca171010d4ed2e6173547b921f42823f82
Drew <drew.faulk12@gmail.com>
2018-09-15 22:08:03 -0500

    Added a client that runs on Vue Js framework.  Client Posts page recieves downloaded pdf.

--- server/app.js
+++ server/app.js
@@ -6,18 +6,30 @@ var pdf = require('html-pdf')
 
 const app = express()
 
+const cors = require('cors')
 
+const fs = require('fs')
+
+app.use(cors())
 app.set('view engine', 'pug')
+app.use(function (req, res, next) {
+  var html_options = {pretty: true, list_obj: "List Object"}
+  var html = pug.renderFile('views/homepage.pug', html_options)
+  var options = {format: 'Letter'}
+  pdf.create(html, options).toFile('./pdf/sample.pdf', function(err, res) {
+    if (err) return //console.log(err)
+    next()
+  })
+})
 app.get('/', (req, res) => {
-    var html_options = {pretty: true, list_obj: "List Object"}
-    var html = pug.renderFile('views/homepage.pug', html_options)
-    var options = {format: 'Letter'}
-    res.render('homepage', {list_obj: "List Object"})
-    pdf.create(html, options).toFile('./pdf/sample.pdf', function(err, res) {
-      if (err) return console.log(err)
-      console.log(res)
-    })
-    console.log(html)
+  res.download(__dirname + '/pdf/sample.pdf', function (err) {
+    console.log(__dirname + '/pdf/sample.pdf')
+    // console.log(err)
+  })
+  // console.log(html)
+  console.log(__dirname + '/pdf/sample.pdf')
+  // res.render('homepage', {list_obj: "List Object"})
+  // res.send({hello: "hello world"})
 })
 
 const port = process.env.PORT || 3000
